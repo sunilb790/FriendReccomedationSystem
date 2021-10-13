bbfcc88e0a7de2cfee26e125891d2de90eaf9179
sunilb790 <sunilbhenspaliya99@gmail.com>
2021-10-13 17:26:30 +0530

    changes in Interest

--- FCSApp/models.py
+++ FCSApp/models.py
@@ -20,15 +20,22 @@ class FriendRelation(models.Model):
     def __str__(self):
         return f'{self.Friend1.user.username} has friendship with {self.Friend2.user.username}'
 
+
 class Interest(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    photography = models.CharField(max_length=100)
-    Healthansfitness = models.CharField(max_length=150)
-    Mentorship = models.CharField(max_length=200)
-    Gardening = models.CharField(max_length=200)
-    Sports = models.CharField(max_length=200)
+    user = models.ManyToManyField(User)
+    TRUE_INTEREST = 'YES'
+    FALSE_INTEREST = 'NO'
+    TRUE_FALSE_IN_INTEREST_CHOICES =[
+        (TRUE_INTEREST,'Yes'),
+        (FALSE_INTEREST,'No'),
+    ]    
+    Photography = models.CharField(max_length=10,choices=TRUE_FALSE_IN_INTEREST_CHOICES,default=FALSE_INTEREST)
+    Healthansfitness = models.CharField(max_length=10,choices=TRUE_FALSE_IN_INTEREST_CHOICES,default=FALSE_INTEREST)
+    Mentorship = models.CharField(max_length=10,choices=TRUE_FALSE_IN_INTEREST_CHOICES,default=FALSE_INTEREST)
+    Gardening = models.CharField(max_length=10,choices=TRUE_FALSE_IN_INTEREST_CHOICES,default=FALSE_INTEREST)
+    Sports = models.CharField(max_length=10,choices=TRUE_FALSE_IN_INTEREST_CHOICES,default=FALSE_INTEREST)
 
     def __str__(self):
         return self.title
